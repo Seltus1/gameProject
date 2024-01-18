73f344b5169f3d8a3a78cf5cb780f2ba9f47522c
DanyDiab <danydiab04@gmail.com>
2024-01-18 16:20:10 -0400

    Test update

--- src/test/java/ProjectileHandlerTest.java
+++ src/test/java/ProjectileHandlerTest.java
@@ -1,23 +1,9 @@
-//import Attacks.Projectile;
-//import Creatures.Enemies.Enemy;
-//import Creatures.Player;
-//import Handlers.EnemyHandler;
-//import Handlers.ProjectileHandler;
-//import org.junit.jupiter.api.Test;
-//import static com.raylib.Raylib.*;
-//import static com.raylib.Jaylib.*;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class ProjectileHandlerTest {
-
-
-//    int gameTicks = 300;
-
 import Attacks.Projectile;
 import Creatures.Enemies.Enemy;
 import Creatures.Player;
 import Handlers.EnemyHandler;
 import Handlers.ProjectileHandler;
+import Handlers.Vector2D;
 import org.junit.jupiter.api.Test;
 import static com.raylib.Raylib.*;
 import static com.raylib.Jaylib.*;
@@ -25,28 +11,59 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ProjectileHandlerTest {
 
-    Player player = new Player(100, 12, 15, 550, 250, 5, 20, 700, RED);
-    Enemy enemy = new Enemy(1, 6, 800, 800, 3, 5, 50, BLUE);
-    Projectile playerProjectile = new Projectile(10,player.getPosX(),player.getPosY(),10,enemy.getPosX(),
-            enemy.getPosY(),"Player", player.getShotRange(),true,BLACK);
-        ProjectileHandler projList = new ProjectileHandler();
+
+    Projectile playerProjectile = new Projectile(10,550,250,10, new Vector2D(550,250, 10)
+            ,"Player", 1000,true,BLACK);
+    ProjectileHandler projList = new ProjectileHandler();
     EnemyHandler enemyHandler = new EnemyHandler();
+    Player player = new Player(100, 12, 15, 550, 250, 5, 20, 700, RED);
+
+
+
     int gameTicks = 300; // 5 seconds for 60 FPS
-//    @Test
-//    void checking_if_poolshot_creates_pool(){
-//        projList.add(playerProjectile);
-//        playerProjectile.setShotTag("Enemy_Pool_Shot");
-//        playerProjectile.setDistanceTravelled(playerProjectile.getMaxRange());
-//        for (int i = 0; i < gameTicks; i++) {
-//            projList.update(enemyHandler, player);
-//        }
-//        Projectile firstP = (Projectile) projList.get(0);
-//        assertEquals("Enemy_Pool",firstP.getShotTag());
-//    }
+
     @Test
-    void test(){
-        assertEquals(10,10);
+    void checking_if_poolshot_creates_pool() {
+        projList.add(playerProjectile);
+        playerProjectile.setShotTag("Enemy_Pool_Shot");
+        playerProjectile.setDistanceTravelled(playerProjectile.getMaxRange());
+        for (int i = 0; i < 1; i++) {
+            projList.update(enemyHandler, player);
+        }
+        Projectile firstP = (Projectile) projList.get(0);
+        assertEquals("Enemy_Pool", firstP.getShotTag());
+    }
+
+    @Test
+    void pool_gets_removed_after_drawTime(){
+        InitWindow(1000,1000,"TEST");
+
+        projList.add(playerProjectile);
+        playerProjectile.setShotTag("Enemy_Pool_Shot");
+        playerProjectile.setDistanceTravelled(playerProjectile.getMaxRange());
+//        iterate for the cooldown (150)
+        for (int i = 0; i < 151; i++) {
+            projList.update(enemyHandler, player);
+        }
+        assertEquals(0, projList.size());
+        CloseWindow();
+    }
+
+    @Test
+    void Player_fire_proj_sets_Enemy_on_fire(){
+        InitWindow(1000,1000,"TEST");
+        Enemy enemy = new Enemy(100, 12, 700, 250, 5, 20, 700, RED);
+        enemyHandler.add(enemy);
+        enemy.setOnFire(false);
+        projList.add(playerProjectile);
+        playerProjectile.setShotTag("Enemy_Fire");
+        playerProjectile.setFinalX(enemy.getPosX());
+        playerProjectile.setFinalY(enemy.getPosY());
+        playerProjectile.createShotLine();
+        for(int i = 0; i < gameTicks; i++) {
+            projList.update(enemyHandler, player);
+        }
+        assertTrue(enemy.isOnFire());
+        CloseWindow();
     }
 }
-
-
--- src/test/java/ProjectileTest.java
+++ src/test/java/ProjectileTest.java
@@ -1,7 +1,5 @@
 import Attacks.Projectile;
-import Creatures.Enemies.Enemy;
-import Creatures.Player;
-import Handlers.ProjectileHandler;
+import Handlers.Vector2D;
 import org.junit.jupiter.api.Test;
 import static com.raylib.Raylib.*;
 import static com.raylib.Jaylib.*;
@@ -9,40 +7,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ProjectileTest {
 
-    Player player = new Player(100, 12, 15, 550, 250, 5, 20, 700, RED);
-    Enemy enemy = new Enemy(1, 6, 800, 800, 3, 5, 50, BLUE);
-    Projectile playerProjectile = new Projectile(10,player.getPosX(),player.getPosY(),10,enemy.getPosX(),
-            enemy.getPosY(),"Player", player.getShotRange(),true,BLACK);
+    Projectile playerProjectile = new Projectile(10,550,250,10, new Vector2D(550,250, 10)
+            ,"Player", 1000,true,BLACK);
+
 
     int gameTicks = 300;
 
     @Test
     void testCheckProjIsOnScreen_PositiveCase() {
+        InitWindow(1000,1000,"TEST");
         playerProjectile.checkProjIsOnScreen();
         assertTrue(playerProjectile.isInBounds());
+        CloseWindow();
     }
 
     @Test
     void testCheckProjIsOnScreen_FalseCase() {
+        InitWindow(1000,1000,"TEST");
         playerProjectile.setPosX(-10);
         playerProjectile.setPosY(-10);
         playerProjectile.checkProjIsOnScreen();
         assertFalse(playerProjectile.isInBounds());
+        CloseWindow();
+
     }
 
     @Test
     void testCheckProjIsOnScreen_True_to_False() {
 //        checking that from a start point on screen that the bounds is true and when moves to a point off the screen
 //        it becomes false
+        InitWindow(1000,1000,"TEST");
+
         playerProjectile.setPosX(10);
         playerProjectile.setPosY(10);
-        playerProjectile.shootLine();
+        playerProjectile.createShotLine();
         for (int i = 0; i < gameTicks; i++) {
             playerProjectile.updateMove();
             playerProjectile.checkProjIsOnScreen();
             assertTrue(playerProjectile.isInBounds());
         }
         assertFalse(playerProjectile.isInBounds());
+        CloseWindow();
+
+
     }
 
 }
