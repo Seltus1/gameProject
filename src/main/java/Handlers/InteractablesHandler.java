c83055f85475162ce1841ef173b00e06d0bb3615
DanyDiab <danydiab04@gmail.com>
2024-06-09 13:15:22 -0300

    working on drawing the building part locations for building quests

--- src/main/java/Handlers/InteractablesHandler.java
+++ src/main/java/Handlers/InteractablesHandler.java
@@ -1,123 +1,128 @@
 package Handlers;
 
 import Creatures.Players.Player;
-import Interactables.Interactable;
+import Interactables.*;
 import com.raylib.Jaylib;
 import com.raylib.Raylib;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-import static com.raylib.Jaylib.BLACK;
-import static com.raylib.Jaylib.RED;
-import static com.raylib.Raylib.*;
-import static com.raylib.Raylib.DrawCircle;
+import static com.raylib.Jaylib.*;
+import static com.raylib.Raylib.Vector2Scale;
 
 public class InteractablesHandler {
     private ArrayList<Interactable> interactables;
     private Interactable interactableToUpdate;
     private VectorHandler distanceVector;
-    private Interactable testInteractable;
-    private Interactable testPower;
+    private AtmosphereAmplifier testAtmo;
+    private PowerGenerator testPower;
     private int defaultInteractableSize;
     private int defaultInteractRadius;
-    private ArrayList<Interactable> telepatherParts;
+    private ArrayList<TelepatherPart> telepatherParts;
     private Random rand;
     private boolean buildingQuestActive;
     private boolean alreadyPicked;
-    private Raylib.Vector2 genPos;
+    private ArrayList<PowerGenerator> activeGens;
+    private ArrayList<PowerGenerator> allGens;
+    private PowerGenerator genLastInteractedWith;
+    private CooldownHandler decreaseGenPowerLevelCH;
+    private int decreaseGenPowerLevelCD;
+    private boolean didSetInteractsToFalse;
+    private SolariumDeposit testSolarium;
 
 
     public InteractablesHandler(Camera2D camera){
         defaultInteractableSize = 20;
         defaultInteractRadius = 70;
         interactables = new ArrayList<>();
+        activeGens = new ArrayList<>();
+        allGens = new ArrayList<>();
         distanceVector = new VectorHandler(0,0,0,camera);
-        testInteractable = new Interactable(defaultInteractableSize,70,2,10,10, camera);
-        testPower = new Interactable(defaultInteractableSize,70,4,100,100,camera);
-        getInteractables().add(testInteractable);
-        getInteractables().add(testPower);
+        decreaseGenPowerLevelCH = new CooldownHandler();
+        decreaseGenPowerLevelCD = 1800;
+        testAtmo = new AtmosphereAmplifier(defaultInteractableSize,70,10,10, camera, this);
+        testPower = new PowerGenerator(defaultInteractableSize,70,100,100,camera, this);
+        testSolarium = new SolariumDeposit(defaultInteractableSize,70,200,200,camera, this);
         telepatherParts = new ArrayList<>();
         rand = new Random();
         startTelepatherTask(camera);
         alreadyPicked = false;
-        genPos = new Raylib.Vector2();
     }
 
     public void update(Player player, GameHandler game, Camera2D camera){
 //        first method also draws the interactables
         determineWhichInteractableToPreformActionOn(player, game);
         preformActionOnInteractable(player, game, camera);
+        if(!activeGens.isEmpty()){
+            for(int i = 0; i < activeGens.size(); i++){
+                decreaseGenPowerLevel(activeGens.get(i));
+//                DrawText(genList.get(i).getCurrentCharge() + "", genList.get(i).getPosX(), genList.get(i).getPosY() - 100, 30, BLACK);
+            }
+        }
         if(buildingQuestActive){
-//            drawBuildingPartsDireciton(telepatherParts, player);
+            drawBuildingPartsDirection(telepatherParts, player);
         }
     }
     private void drawInteractables(Interactable interactable){
-        DrawCircle(interactable.getPosX(),interactable.getPosY(),interactable.getSize(),BLACK);
+        DrawCircle(interactable.getPosX(),interactable.getPosY(),interactable.getSize(),interactable.getColor());
     }
-    private void drawBuildingPartsDireciton(ArrayList<Interactable> partList, Player player){
-        for(int i = 0; i < partList.size(); i ++){
-//          gpt cooking lol (it did not cook)
 
-//            int interactableY = partList.get(i).getPosY();
-//            int interactableX = partList.get(i).getPosX();
-//            double angle = Math.atan2(interactableY - GetScreenHeight() / 2, interactableX - GetScreenWidth() / 2);
-//            double maxDistance = Math.min(GetScreenWidth(), GetScreenHeight()) / 2;
-//            int triangleSize = 50;
-//            int[] xPoints = {
-//                    (int) (GetScreenWidth() / 2 + triangleSize * Math.cos(angle)),
-//                    (int) (GetScreenWidth()  / 2 + triangleSize * Math.cos(angle + 2 * Math.PI / 3)),
-//                    (int) (GetScreenWidth()  / 2 + triangleSize * Math.cos(angle + 4 * Math.PI / 3))
-//            };
-//            int[] yPoints = {
-//                    (int) (GetScreenHeight() / 2 + triangleSize * Math.sin(angle)),
-//                    (int) (GetScreenHeight()  / 2 + triangleSize * Math.sin(angle + 2 * Math.PI / 3)),
-//                    (int) (GetScreenHeight()  / 2 + triangleSize * Math.sin(angle + 4 * Math.PI / 3))
-//            };
-//            Raylib.Vector2 point1 = new Raylib.Vector2(new Jaylib.Vector2(xPoints[0], yPoints[0]));
-//            Raylib.Vector2 point2 = new Raylib.Vector2(new Jaylib.Vector2(xPoints[1], yPoints[1]));
-//            Raylib.Vector2 point3 = new Raylib.Vector2(new Jaylib.Vector2(xPoints[2], yPoints[2]));
-//            DrawTriangle(point1,point2,point3, RED);
-//            DrawTriangle(point3,point2,point1, RED);
+    private void drawBuildingPartsDirection(ArrayList<TelepatherPart> partList, Player player){
+        for(int i = 0; i < partList.size();i++){
+//            find the direction of the vector from the players position to the position of the part
+            Raylib.Vector2 direction = Vector2Subtract(partList.get(i).getPos(), player.getPosition());
+//            normalize that vector to make it a unit vector
+            Raylib.Vector2 normalizedDir = Vector2Normalize(direction);
+//            multiply that vector by a scalar of 100
+            Raylib.Vector2 scaledDir = Vector2Scale(normalizedDir, 200);
+//            add that vector to the players position so it follows the players position
+            Raylib.Vector2 endPoint = Vector2Add(scaledDir,player.getPosition());
 
 
 
+//            draw a line from the players position to the endpoint
+            DrawLineV(player.getPosition(),endPoint,BLACK);
 
-//            want to change this from drawing a line to the interactable
-//            to drawing a triangle marking the direction towards the interactable
-//            make the triangle bigger the further the distance.
-//            if the interactable is the on the screen, don't draw the triangle
-            if(distanceVector.distanceBetweenTwoObjects(player.getPosition(), partList.get(i).getPos()) > 800) {
-                DrawLineV(player.getPosition(), partList.get(i).getPos(), BLACK);
-            }
         }
-
     }
-//    want to make this function be able to switch between differnt interactables.
+
+
+
+
+
+
+
+
+
+
+    //    want to make this function be able to switch between differnt interactables.
 //    for example if you are interacting with one thing, then get in range with another,
 //    then press F it should revert the action of the first and then preform the action on the 2nd
     private void determineWhichInteractableToPreformActionOn(Player player, GameHandler game){
         for (int i = 0; i < getInteractables().size(); i++) {
             Interactable current = getInteractables().get(i);
-            drawInteractables(current);
             if(current.isHasPower()){
                 current.drawPowerLineBetweenPoweredInteractableAndGen(this);
             }
-            DrawText(current.isHasPower() + "", current.getPosX(), current.getPosY() - 100, 30, BLACK);
+            drawInteractables(current);
             if (distanceVector.distanceBetweenTwoObjects(current.getPos(), player.getPosition()) < current.getInteractRadius()) {
                 current.setInRange(true);
+
                 if(IsKeyPressed(KEY_F)) {
-                    current.updateActionStatus(player, game);
+                    current.updateActionStatus(player, game, this);
                     if(interactableToUpdate == null && !alreadyPicked){
                         interactableToUpdate = current;
                         alreadyPicked = true;
                     }
+
                     else if(alreadyPicked && interactableToUpdate != null){
                         int pos1 = distanceVector.distanceBetweenTwoObjects(player.getPosition(),current.getPos());
                         int pos2 = distanceVector.distanceBetweenTwoObjects(player.getPosition(),interactableToUpdate.getPos());
                         if(pos1 < pos2){
+                            current.updateActionStatus(player,game, this);
                             interactableToUpdate = current;
-                            current.updateActionStatus(player,game);
+                            current.updateActionStatus(player,game, this);
                         }
                     }
                     else {
@@ -136,9 +141,11 @@ public class InteractablesHandler {
         if(interactableToUpdate != null){
 //          if there is an interactable to update, update it.
             interactableToUpdate.update(player, game, this, camera);
-            if(!interactableToUpdate.isInRange() && interactableToUpdate.getType() != 4){
+            if(!interactableToUpdate.isInRange() && !player.isConnectingPowerLine() && !interactableToUpdate.isExpandBorder()){
 //               set the values back to false/revert the actions that the interactable does
-                interactableToUpdate.updateActionStatus(player,game);
+                if(interactableToUpdate.isBeingUpdated()){
+                    interactableToUpdate.updateActionStatus(player, game, this);
+                }
 //                if the interactable is not longer in range, dont update it anymore
                 interactableToUpdate = null;
                 alreadyPicked = false;
@@ -149,14 +156,28 @@ public class InteractablesHandler {
     public void startTelepatherTask(Camera2D camera){
         buildingQuestActive = true;
         for(int i = 0; i < 3; i++){
-//            int randPosX = rand.nextInt(4000) - 2000;
-//            int randPosY = rand.nextInt(4000) - 2000;
-            int randPosX = rand.nextInt(400) - 200;
-            int randPosY = rand.nextInt(400) - 200;
+            int randPosX = rand.nextInt(4000) - 2000;
+            int randPosY = rand.nextInt(4000) - 2000;
+//            int randPosX = rand.nextInt(400) - 200;
+//            int randPosY = rand.nextInt(400) - 200;
             Raylib.Vector2 partPos = new Raylib.Vector2(new Jaylib.Vector2(randPosX,randPosY));
-            Interactable telepatherPart = new Interactable(getDefaultInteractableSize(),getDefaultInteractRadius(),1,(int) partPos.x(), (int) partPos.y(), camera);
+            TelepatherPart telepatherPart = new TelepatherPart(getDefaultInteractableSize(),getDefaultInteractRadius(),(int) partPos.x(), (int) partPos.y(), camera, this);
             telepatherParts.add(telepatherPart);
-            getInteractables().add(telepatherPart);
+        }
+    }
+    private void decreaseGenPowerLevel(PowerGenerator gen){
+        if(gen.getCurrentCharge() > 0){
+            didSetInteractsToFalse = false;
+            if(decreaseGenPowerLevelCH.cooldown(decreaseGenPowerLevelCD)){
+                gen.setCurrentCharge(gen.getCurrentCharge() - 1);
+            }
+        }
+        else if(!didSetInteractsToFalse){
+            didSetInteractsToFalse = true;
+            for(int i = 0; i < gen.getPoweringList().size(); i++){
+                gen.getPoweringList().get(i).setHasPower(false);
+
+            }
         }
     }
 
@@ -193,11 +214,11 @@ public class InteractablesHandler {
         this.defaultInteractRadius = defaultInteractRadius;
     }
 
-    public ArrayList<Interactable> getTelepatherParts() {
+    public ArrayList<TelepatherPart> getTelepatherParts() {
         return telepatherParts;
     }
 
-    public void setTelepatherParts(ArrayList<Interactable> telepatherParts) {
+    public void setTelepatherParts(ArrayList<TelepatherPart> telepatherParts) {
         this.telepatherParts = telepatherParts;
     }
 
@@ -217,11 +238,23 @@ public class InteractablesHandler {
         this.alreadyPicked = alreadyPicked;
     }
 
-    public Vector2 getGenPos() {
-        return genPos;
+    public ArrayList<PowerGenerator> getActiveGens() {
+        return activeGens;
     }
 
-    public void setGenPos(Vector2 genPos) {
-        this.genPos = genPos;
+    public PowerGenerator getGenLastInteractedWith() {
+        return genLastInteractedWith;
+    }
+
+    public void setGenLastInteractedWith(PowerGenerator genLastInteractedWith) {
+        this.genLastInteractedWith = genLastInteractedWith;
+    }
+
+    public ArrayList<PowerGenerator> getAllGens() {
+        return allGens;
+    }
+
+    public void setAllGens(ArrayList<PowerGenerator> allGens) {
+        this.allGens = allGens;
     }
 }
